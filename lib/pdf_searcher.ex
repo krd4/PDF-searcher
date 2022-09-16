fe0c6998419c7d1f783bba4dba7d8a41f1fa0656
kryousuke <k000ryousuke@gmail.com>
2022-09-16 16:09:34 +0900

    init mix

---
+++ lib/pdf_searcher.ex
@@ -0,0 +1,18 @@
+defmodule PdfSearcher do
+  @moduledoc """
+  Documentation for `PdfSearcher`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PdfSearcher.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pdf_searcher_test.exs
@@ -0,0 +1,8 @@
+defmodule PdfSearcherTest do
+  use ExUnit.Case
+  doctest PdfSearcher
+
+  test "greets the world" do
+    assert PdfSearcher.hello() == :world
+  end
+end
